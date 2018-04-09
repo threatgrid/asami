618594ddea1472f0a552a35f07165b163917247c
Paula Gearon <pgearon@cisco.com>
2018-04-09 19:19:30 -0400

    updated dependency to deployed clojar

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject asami "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An in-memory implementation of a graph store, implementing the naga-store protocol"
+  :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [prismatic/schema "1.1.7"] 
                  [org.clojure/core.cache "0.7.1"]
-                 [naga-store "0.0.1-SNAPSHOT"]]
+                 [naga-store "0.0.1"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {
     :builds {
