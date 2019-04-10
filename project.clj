40f0fe1b1db48a201828c78a6b838156b1a8f4ac
Paula Gearon <pgearon@cisco.com>
2019-04-10 17:53:48 -0400

    Merged subtraction and binding query operations. Release 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/asami "0.2.18-SNAPSHOT"
+(defproject org.clojars.quoll/asami "0.3.0"
   :description "An in-memory implementation of a graph store, implementing the naga-store protocol"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
                  [prismatic/schema "1.1.10"] 
-                 [org.clojure/core.cache "0.7.1"]
+                 [org.clojure/core.cache "0.7.2"]
                  [org.clojars.quoll/naga-store "0.3.0"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {
