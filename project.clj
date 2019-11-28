f4b32a7537f385542e73a14a91117ca7d00a0108
Paula Gearon <pgearon@cisco.com>
2019-11-27 23:05:10 -0500

    Updated dependencies for final release

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject org.clojars.quoll/asami "0.3.6"
+(defproject org.clojars.quoll/asami "0.4.0"
   :description "An in-memory implementation of a graph store, implementing the naga-store protocol"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [prismatic/schema "1.1.10"] 
-                 [org.clojure/core.cache "0.7.2"]
-                 [org.clojars.quoll/naga-store "0.3.3"]]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [prismatic/schema "1.1.12"] 
+                 [org.clojure/core.cache "0.8.2"]
+                 [org.clojars.quoll/naga-store "0.3.4"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {
     :builds {
