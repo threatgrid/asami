680aa7e1e4809c4966932c8beb4734cd5228e1a4
Paula Gearon <pgearon@cisco.com>
2021-02-18 13:58:48 -0500

    Removing ClojureScript from the dependency tree of the artifacts

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
-(defproject org.clojars.quoll/asami "1.2.13"
+(defproject org.clojars.quoll/asami "1.2.14"
   :description "An in memory graph store for Clojure and ClojureScript"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.2"]
-                 [org.clojure/clojurescript "1.10.773"]
                  [prismatic/schema "1.1.12"] 
                  [org.clojure/core.cache "0.8.2"]
-                 [org.clojars.quoll/zuko "0.4.0"]]
-  :plugins [[lein-cljsbuild "1.1.7"]
+                 [org.clojars.quoll/zuko "0.4.1"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
             [cider/cider-nrepl "0.24.0"]]
+  :profiles {
+    :dev {
+      :dependencies [[org.clojure/clojurescript "1.10.773"]]}}
   :cljsbuild {
     :builds {
       :dev
