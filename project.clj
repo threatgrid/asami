00a1c16ce1d778a6748f4aed7d237644e26439b3
Paula Gearon <pgearon@cisco.com>
2021-03-10 15:13:45 -0500

    Alpha3 updated

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/asami "2.0.0-alpha2"
+(defproject org.clojars.quoll/asami "2.0.0-alpha3"
   :description "An in memory graph store for Clojure and ClojureScript"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
