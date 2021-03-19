1b1018e4f77faf82e492c3e6b00d8276e9d6a3f8
Paula Gearon <pgearon@cisco.com>
2021-03-18 22:38:08 -0400

    Bumping release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/asami "2.0.0-alpha4"
+(defproject org.clojars.quoll/asami "2.0.0-alpha5"
   :description "An in memory graph store for Clojure and ClojureScript"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
