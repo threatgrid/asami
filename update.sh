f7c231ffc0329fc15a6ce2cc8da3f82d729765cf
Paula Gearon <pgearon@cisco.com>
2021-04-28 14:30:08 -0400

    A script to move files from < 2.0.1 into the new positions for 2.0.2

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mv ${1}/avet.idx_stmt.bin ${1}/avet_stmt.bin
+mv ${1}/avet.idx_stmtidx.bin ${1}/avet_stmtidx.bin
+mv ${1}/eavt.idx_stmt.bin ${1}/eavt_stmt.bin
+mv ${1}/eavt.idx_stmtidx.bin ${1}/eavt_stmtidx.bin
+mv ${1}/veat.idx_stmt.bin ${1}/veat_stmt.bin
+mv ${1}/veat.idx_stmtidx.bin ${1}/veat_stmtidx.bin
+
