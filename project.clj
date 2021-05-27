ae642b05b50a1706f9fcf809186fbb0ed6a28677
Paula Gearon <pgearon@cisco.com>
2021-05-27 15:06:11 -0400

    Updated to Zuko 0.6.5, to avoid recursion processing entity arrays

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/asami "2.0.4"
+(defproject org.clojars.quoll/asami "2.0.5"
   :description "An in memory graph store for Clojure and ClojureScript"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
@@ -6,13 +6,13 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [prismatic/schema "1.1.12"] 
                  [org.clojure/core.cache "1.0.207"]
-                 [org.clojars.quoll/zuko "0.6.4"]
+                 [org.clojars.quoll/zuko "0.6.5"]
                  [org.clojure/data.priority-map "1.0.0"]
                  [tailrecursion/cljs-priority-map "1.2.1"]]
   :plugins [[lein-cljsbuild "1.1.8"]]
   :profiles {
     :dev {
-      :dependencies [[org.clojure/clojurescript "1.10.844"]]}
+      :dependencies [[org.clojure/clojurescript "1.10.866"]]}
     :uberjar {
       :aot [asami.peer]}}
   :cljsbuild {
