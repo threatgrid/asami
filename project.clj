e5b1c8ce7c9e8cee5e14276265036cb61050e292
Paula Gearon <pgearon@cisco.com>
2021-06-09 23:32:14 -0400

    Added a native command line utility

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quoll/asami "2.0.5"
+(defproject org.clojars.quoll/asami "2.0.6-SNAPSHOT"
   :description "An in memory graph store for Clojure and ClojureScript"
   :url "http://github.com/threatgrid/asami"
   :license {:name "Eclipse Public License"
@@ -11,19 +11,25 @@
                  [org.clojure/data.priority-map "1.0.0"]
                  [tailrecursion/cljs-priority-map "1.2.1"]]
   :plugins [[lein-cljsbuild "1.1.8"]]
-  :profiles {
-    :dev {
-      :dependencies [[org.clojure/clojurescript "1.10.866"]]}
-    :uberjar {
-      :aot [asami.peer]}}
-  :cljsbuild {
-    :builds {
-      :dev {:source-paths ["src"]
-            :compiler {:output-to "out/asami/core.js"
-                       :optimizations :simple
-                       :pretty-print true}}
-      :test {:source-paths ["src" "test"]
-             :compiler {:output-to "out/asami/test_memory.js"
-                        :optimizations :simple
-                        :pretty-print true}}}
-  :test-commands {"unit" ["node" "out/asami/test_memory.js"]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.866"]]}
+             :uberjar {:aot [asami.peer]}
+             :native {:plugins [[lein-shell "0.5.0"]]
+                      :source-paths ["src" "src-native"]
+                      :aot :all
+                      :main asami.main
+                      :dependencies [[cheshire "5.10.0"]]
+                      :aliases {"native" ["shell"
+                                          "native-image" "--report-unsupported-elements-at-runtime"
+                                          "--initialize-at-build-time" "--no-server"
+                                          "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
+                                          "-H:Name=./target/${:name}"
+                                          "-H:TraceClassInitialization=\"java.io.FilePermission\""]}}}
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
+                             :compiler {:output-to "out/asami/core.js"
+                                        :optimizations :simple
+                                        :pretty-print true}}
+                       :test {:source-paths ["src" "test"]
+                              :compiler {:output-to "out/asami/test_memory.js"
+                                         :optimizations :simple
+                                         :pretty-print true}}}
+              :test-commands {"unit" ["node" "out/asami/test_memory.js"]}})
---
+++ src-native/asami/main.clj
@@ -0,0 +1,72 @@
+(ns ^{:doc "Entry point for CLI"
+      :author "Paula Gearon"}
+  asami.main
+  (:require [clojure.string :as s]
+            [clojure.edn :as edn]
+            [cheshire.core :as json]
+            [clojure.pprint :refer [pprint]]
+            [asami.core :as asami])
+  (:gen-class))
+
+(defn process-args
+  [args]
+  (loop [result {:query "-"} [a & [arg & rargs :as rem] :as args] args]
+    (if-not (seq args)
+      result
+      (let [[r more] (cond
+                       (#{"-e" "-q"} a) [(assoc result :query arg) rargs]
+                       (= a "-f") [(assoc result :file arg) rargs]
+                       (#{"-?" "--help"} a) [(assoc result :help true) rem]
+                       (s/starts-with? a "asami:") [(assoc result :url a) rem])]
+        (recur r more)))))
+
+(defn read-data-file
+  [f]
+  (let [text (slurp (if (= f "-") *in* f))]
+    (if (s/ends-with? f ".json")
+      (json/parse-string text)
+      (edn/read-string text))))
+
+(defn load-data-file
+  [conn f]
+  (:db-after @(asami/transact conn {:tx-data (read-data-file f)})))
+
+(defn read-queries
+  [q]
+  (let [text (s/trim (if (= q "-") (slurp *in*) q))
+        mtext (s/split text #";")
+        all (mapv (fn [text]
+                    (let [txt (if (= \: (first (s/trim text))) (str "[" text "]") text)]
+                      (edn/read-string txt)))
+                  mtext)]
+    (if (= 1 (count all))
+      (let [[qv] all]
+        (if (vector? (first qv)) qv all))
+      all)))
+
+(defn print-usage
+  []
+  (println "Usage: asami URL [-f filename] [-e query] [--help | -?]")
+  (println "URL: the URL of the database to use. Must start with asami:mem://, asami:multi:// or asami:local://")
+  (println "-f filename: loads the filename into the database. A filename of \"-\" will use stdin.")
+  (println "             Data defaults to EDN. Filenames ending in .json are treated as JSON.")
+  (println "-e query: executes a query. Not specifying, or using \"-\" will read from stdin instead of a command line argument.")
+  (println "          Multiple queries can be specified as edn (vector of query vectors) or ; separated.")
+  (println "-? | --help: This help"))
+
+(defn -main
+  [& args]
+  (let [{:keys [query file url help]} (process-args args)]
+    (when help
+      (print-usage)
+      (System/exit 0))
+    (when-not url
+      (println "Database URL must be specified")
+      (System/exit 1))
+    (let [conn (asami/connect url)
+          db (if file
+               (load-data-file conn file)
+               (asami/db conn))]
+      (doseq [query (read-queries query)]
+        (pprint (asami/q query db)))
+      (System/exit 0))))
