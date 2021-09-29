be5d5160ecda52e158cf9d59d4e6ddfd6db10dc4
Paula Gearon <pgearon@cisco.com>
2021-09-28 22:03:06 -0400

    now allows multiple ; separated queries, and will also execute unwrapped queries when no ; is present

--- src-native/asami/main.clj
+++ src-native/asami/main.clj
@@ -60,61 +60,96 @@
   :name "asami.PBR"
   :extends java.io.PushbackReader
   :prefix "pbr-"
+  :init init
+  :state qstate
+  :constructors {[java.io.Reader clojure.lang.IDeref] [java.io.Reader]}
   :exposes-methods {read readSuper})
 
+(defn pbr-init
+  [reader query-acc]
+  [[reader] {:dbl-newline (atom false)
+             :query-acc query-acc}])
+
 (defn pbr-read
-  [this]
-  (let [c (.readSuper ^asami.PBR this)]
-    (if (= (int \;) c)
-      0
-      c)))
+  "Returns characters from the reader for an edn parser.
+  When queries are not being accumulated, then pass through"
+  [^asami.PBR this]
+  (let [{:keys [dbl-newline query-acc]} (.qstate this)]
+    (if-not @query-acc 
+      (.readSuper this)  ;; not accumulating. Pass through.
 
-(defn derive-stream [{:keys [interactive? query]}]
-  (let [input (if (or (= query "-") interactive?)
-                *in*
-                (.getBytes ^String query))]
-    (asami.PBR. (io/reader input))))
+      ;; check if this is returning 2 characters after a newline was pressed.
+      (if @dbl-newline
+        (do ;; The first character was already returned.
+          (reset! dbl-newline false)
+          (int \newline))
+        (let [c (.readSuper this)]
+          (case c   ;; intercept \; and \newline characters
+            59 0    ;; \; character
+            10 (do  ;; \newline character
+                 (swap! dbl-newline not)  ;; remember that a second character will be needed
+                 0)  ;; This is the first of 2 characters returned
+            c))))))  ;; pass through by default
 
-(defn separator? [s]
+(defn derive-input
+  "Convert command line options into the input stream required"
+  [{:keys [interactive? query]}]
+  (if (or (= query "-") interactive?)
+    *in*
+    (.getBytes ^String query)))
+
+(defn separator?
+  "Check for a magic 'empty' symbol that is used to indicate a query separator"
+  [s]
   (= (symbol "\0") s))
 
-(defn repl [stream db prompt]
+(defn repl [input db prompt]
+        ;; function to print a prompt after every newline
   (let [prompt-fn (if (some? prompt)
                     (fn [] (print prompt) (flush))
                     (constantly nil))
+        ;; executes a parsed edn query and prints the result
         execute (fn [query]
                   (try
                     (pprint (asami/q query db))
                     (catch Exception e
-                      (printf "Error executing query %s: %s\n" (pr-str query) (ex-message e)))))]
-    (loop [q nil]
-      (when-not q (prompt-fn))
+                      (printf "Error executing query %s: %s\n" (pr-str query) (ex-message e)))))
+        ;; maintain state to accumulate queries not wrapped in an edn structure
+        query-acc (atom nil)
+        ;; create the input reader, providing the query accumulation state
+        stream (asami.PBR. (io/reader input) query-acc)]
+    (loop []
+      ;; prompt when necessary
+      (when-not @query-acc (prompt-fn))
+      ;; get the next
       (let [query (try (edn/read reader-opts stream) (catch Exception e e))]
         (cond
           (instance? Exception query)
           (do (printf "Error: %s\n" (ex-message query))
               (println "Type: " (type query))
               (.printStackTrace ^Exception query)
-              ; (recur nil)
-              nil
-              )
+              nil)
 
           (eof? query)
-          (when q
+          (when-let [q @query-acc]
             (execute q)
             nil)
 
           (separator? query) 
-          (do (execute q)
-              (recur nil))
+          (do (execute @query-acc)
+              (reset! query-acc nil)
+              (recur))
 
           :else
-          (if q    ;; check if a query is being accumulated
-            (recur (conj q query))  ;; add to the accumulated query
-            (if (or (sequential? query) (map? query))  ;; a complete query structure
-              (do (execute query)
-                  (recur nil))
-              (recur [query]))))))))  ;; otherwise, start accumulating a new query
+          (do
+            (if @query-acc            ;; check if a query is being accumulated
+              (swap! query-acc conj query) ;; add to the accumulated query
+
+              ;; else, a complete query structure
+              (if (or (sequential? query) (map? query))  ;; check if this a complete query
+                (execute query)
+                (reset! query-acc [query])))  ;; otherwise, start accumulating a new query
+            (recur)))))))
 
 (defn print-usage
   []
@@ -146,10 +181,10 @@
       (System/exit 1))
 
     (let [db (derive-database options)
-          stream (derive-stream options)
+          input (derive-input options)
           prompt (when interactive?
-                   (println "Queries may be multi-line. Unwrapped queries must be terminated with ;")
-                   "?- ")]
-      (repl stream db prompt))
+                   (println "Unwrapped queries may be separated with ;")
+                    "?- ")]
+      (repl input db prompt))
 
     (System/exit 0)))
